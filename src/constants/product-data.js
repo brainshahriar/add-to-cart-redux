4aa22879925f55475d71c9ad6f1e6fd69cbdab6d
shahriar <shahriar.mehedi@bjitgroup.com>
2022-09-08 17:23:38 +0600

    image

--- src/constants/product-data.js
+++ src/constants/product-data.js
@@ -13,7 +13,7 @@ export const data = [
     description: "something 2",
     price: 5,
     image:
-      "https://images.pexels.com/photos/90946/pexels-photo-90946.jpeg?cs=srgb&dl=pexels-math-90946.jpg&fm=jpg",
+      "https://images.unsplash.com/photo-1505740420928-5e560c06d30e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8cHJvZHVjdHxlbnwwfHwwfHw%3D&w=1000&q=80",
   },
   {
     _id: "3",
@@ -21,6 +21,6 @@ export const data = [
     description: "something 3",
     price: 12,
     image:
-      "https://images.pexels.com/photos/90946/pexels-photo-90946.jpeg?cs=srgb&dl=pexels-math-90946.jpg&fm=jpg",
+      "https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZHVjdHxlbnwwfHwwfHw%3D&w=1000&q=80",
   },
 ];
